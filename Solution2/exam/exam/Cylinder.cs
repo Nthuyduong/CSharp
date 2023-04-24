698251332ef45f31d1e6990163fe079b3b98ec3c
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-04-24 19:02:54 +0700

    submit

---
+++ Solution2/exam/exam/Cylinder.cs
@@ -0,0 +1,38 @@
+﻿namespace exam;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Cylinder
+{
+    public double radius;
+    public double height;
+    public double baseArea;
+    public double lateralArea;
+    public double totalArea;
+    public double volume;
+
+    public Cylinder()
+    {
+
+    }
+
+    public Cylinder(double radius, double height, double baseArea,double lateralArea, double totalArea, double volume)
+    {
+        this.radius = radius;
+        this.height = height;
+        this
+    }
+    public void Process()
+        {
+
+        }
+
+        public void Result()
+        {
+
+        }
+    
+}
+
