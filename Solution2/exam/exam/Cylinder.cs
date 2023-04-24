5b40ed203c8b3dd03c45c483086e2815ab76b888
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-04-24 19:17:12 +0700

    commit

--- Solution2/exam/exam/Cylinder.cs
+++ Solution2/exam/exam/Cylinder.cs
@@ -22,17 +22,37 @@ class Cylinder
     {
         this.radius = radius;
         this.height = height;
-        this
+        this.baseArea = baseArea;
+        this.lateralArea = lateralArea;
+        this.totalArea = totalArea;
+        this.volume = volume;
     }
-    public void Process()
-        {
 
-        }
+    public void Input()
+    {
+        Console.WriteLine("Enter radius");
+        this.radius = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter height");
+        this.height = double.Parse(Console.ReadLine());
+    }
 
-        public void Result()
-        {
+     public void Process()
+    {
+        this.baseArea = 3.14 * (this.radius * this.radius);
+        this.lateralArea = 2 * 3.14 * this.radius * this.height;
+        this.totalArea = (2 * this.baseArea) + this.lateralArea;
+        this.volume = this.baseArea * this.height;
 
-        }
+      }
+
+    public string Result()
+     {
+        string result = "Base Area: " + this.baseArea + "\n";
+        result += "Lateral Area: " + this.lateralArea + "\n";
+        result += "Total Area: " + this.totalArea + "\n";
+        result += "Volume: " + this.volume + "\n";
+        return result;
+    }
     
 }
 
