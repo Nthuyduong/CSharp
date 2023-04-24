2e720974cd501c9a952b1d52f3ef7a51a7a0bc40
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-04-24 13:27:26 +0700

    commit

--- Solution2/Abstract/NVKinhdoanh.cs
+++ Solution2/Abstract/NVKinhdoanh.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Abstract
 {
-	public class NVKinhdoanh
+	public class NVKinhdoanh : Nhavien
 	{
+		public double Doanhthu;
+
 		public NVKinhdoanh()
 		{
+
+		}
+
+		public NVKinhdoanh(String Hoten, double LuongCB, double Doanhthu)
+            : base(string Hoten, double LuongCB)
+        {
+			this.Doanhthu = Doanhthu;
+		}
+
+		public override void Nhap()
+		{
+			base:Nhap();
+			Console.WriteLine("Nhap so doanh thu:");
+			Doanhthu = double.Prase(Console.ReadLine());
+		}
+
+		public override void Hienthi()
+		{
+			base:Hienthi();
+			Console.WriteLine("So doanh thu: {0}", Doanhthu);
+		}
+
+		public override double Tinhluong()
+		{
+			return LuongCB + Doanhthu * 10 / 100;
 		}
 	}
 }
--- Solution2/Abstract/NVKythuat.cs
+++ Solution2/Abstract/NVKythuat.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Abstract
 {
-	public class EmptyClass
+	public class NVKythuat : Nhanvien
 	{
-		public EmptyClass()
+		public int ngaycong;
+		public NVKythuat();
 		{
+
+		}
+
+		public NVKythuat(String Hoten, double LuongCB, int ngaycong)
+		:base(Hoten, LuongCB)
+		{
+			this.ngaycong = ngaycong;
+		}
+
+		public override void Nhap()
+		{
+			base:Nhap();
+			Console.Write("Nhap so ngay cong:")
+			ngaycong.int.Parse(Console.Readline());
+		}
+
+		public override void Hienthi()
+		{
+			base.Hienthi();
+			Console.WriteLine("So ngay cong: {0}", ngaycong);
+		}
+
+		public override double Tinhluong()
+		{
+			return LuongCB + (ngaycong * 100000);
 		}
 	}
 }
