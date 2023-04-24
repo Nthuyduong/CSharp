0c0ba59491a81a4ff4973a371cc13669c2f19acd
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-04-24 19:26:04 +0700

    commit

---
+++ Solution2/Application/Application/Demo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Application;
+
+namespace exam
+{
+    public class Demo
+    {
+        public static void Main(string[] args)
+        {
+            Cylinder c = new Cylinder();
+            c.Input();
+            c.Process();
+            Console.WriteLine(c.Result());
+        }
+
+    }
+}
+
+
