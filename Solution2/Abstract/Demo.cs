25a02c145d46e3232352e2432282607e540b79ea
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-04-24 16:29:52 +0700

    commit

--- Solution2/Abstract/Demo.cs
+++ Solution2/Abstract/Demo.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 namespace Abstract
 {
-	public class EmptyClass
+	public class Demo
 	{
-		public EmptyClass()
+		public static void Main(string[] args)
 		{
-		}
+			//nhan vien ky thuat
+			Nhanvien nv1 = new NVKythuat;
+			nv1.Nhap();
+			nv1.Hienthi();
+
+			//nhan vien kinh doanh
+            Nhanvien nv2 = new NVKinhdoanh;
+			nv2.Nhap();
+			nv2.Hienthi();
+        }
 	}
 }
 
