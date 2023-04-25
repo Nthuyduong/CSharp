f44052fcf42f648209fcfa399d483ce2c7539efe
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-04-25 08:23:12 +0700

    commit

---
+++ Solution2/Finalexam/Finalexam/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Finalexam
+{
+	public class Product
+	{
+		public string ProductID;
+		public string Name;
+		public double Price;
+		public Product()
+		{
+		}
+
+		public Product(string ProductID, string Name, double Price)
+		{
+			this.ProductID = ProductID;
+			this.Name = Name;
+			this.Price = Price;
+		}
+
+		public void Add()
+		{
+			Console.WriteLine("Enter product ID:");
+			ProductID = Console.ReadLine();
+            Console.WriteLine("Enter product's name:");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter product's price:");
+            Price = double.Parse(Console.ReadLine());
+        }
+	}
+}
+
