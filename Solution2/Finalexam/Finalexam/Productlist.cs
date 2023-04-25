7a57cccebfb6ebe74e33d6ade06d45f0aa52cf4e
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-04-25 09:58:37 +0700

    commit

---
+++ Solution2/Finalexam/Finalexam/Productlist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finalexam
+{
+	public class Productlist
+	{
+		Product[] list;
+        int n;
+
+        public Productlist()
+        {
+            n = 0;
+            list = new Product[10];
+        }
+
+        public void Addproduct()
+        {
+
+        }
+
+       public void Displayproduct()
+        {
+
+        }
+    }
+}
+
