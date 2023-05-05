c8bc7eff3ff1c5de4994277e9be2ed4fc0e827c3
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-05-05 16:41:30 +0700

    commit

---
+++ Solution2/Exam_/Phone.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Exam_CSharp
+{
+	public class Phone
+	{
+		public Phone()
+		{
+		}
+	}
+}
+
---
+++ Solution2/Exam_/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_CSharp;
+abstract class Product
+{
+    public string ProductID;
+    public string ProductName;
+    public int Price;
+
+    public Product()
+    {
+
+    }
+
+    public Product(string ProductID, string ProductName, int Price)
+    {
+        this.ProductID = ProductID;
+        this.ProductName = ProductName;
+        this.Price = Price;
+    }
+
+    public abstract double computeTax();
+}
+
